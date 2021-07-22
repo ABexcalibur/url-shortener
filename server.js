6400e502730e07a493a4bb803a0f29ad4c860ca5
ABexcalibur <87257587+ABexcalibur@users.noreply.github.com>
2021-07-22 15:14:06 +0530

    update

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express();
 
-mongoose.connect('mongodb://localhost/urlShortener', {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
+const db = 'mongodb+srv://saumya:ss%40123456@cluster0.4iave.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.Promise = global.Promise;
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology:true })
+  .then(res => console.log("Connected to DB"))
+  .catch(err => console.log(err))
+
+//mongoose.connect('mongodb://localhost/urlShortener', {
+//    useNewUrlParser: true, useUnifiedTopology: true
+//})
 
 port = process.env.PORT || 3000;
 
